3e099d7ed79916226a2fa7857ce6edb6a505f7b2
RyanBeatty <rvbeatty@email.wm.edu>
2014-09-11 13:57:09 -0400

    initial connect code made

--- upi.go
+++ upi.go
@@ -5,9 +5,26 @@ import (
 	"fmt"
 )
 
-func Connect(address string) net.Conn {
-	address += ":21"
-	conn, err := net.Dial("tcp", address)
-	RaiseOnError(err, fmt.Sprintf("error: can't connect to <%s>", address))
-	return conn
+type UPI interface {
+	Connect(address, port string) error
+	Close()
+	
 }
+
+type Upi struct {
+	Conn net.Conn
+}
+
+func (upi Upi) Connect(address string, port string) error{
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", address, port))
+	if err != nil {
+		return err
+	}
+	upi.Conn = conn
+	return err
+}
+
+func (upi Upi) Close() {
+	upi.Conn.Close()
+}
+
